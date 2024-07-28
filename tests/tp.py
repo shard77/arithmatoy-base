484534bb33ea715e9a71bd2faf43e4afaefcffb2
shard77 <sh7user@gmail.com>
2024-07-28 17:24:18 +0200

    feat: complete tp.py

--- tests/tp.py
+++ tests/tp.py
@@ -2,44 +2,64 @@
 
 
 def nombre_entier(n: int) -> str:
-    pass
+    return "S" * n + "0"
 
 
 def S(n: str) -> str:
-    pass
+    return "S" + n
 
 
 def addition(a: str, b: str) -> str:
-    pass
+    if a == "0":
+        return b
+    return S(addition(a[1:], b))
 
 
 def multiplication(a: str, b: str) -> str:
-    pass
+    if a == "0" or b == "0":
+        return "0"
+    return addition(b, multiplication(a[1:], b))
 
 
 def facto_ite(n: int) -> int:
-    pass
+    if n == 0:
+        return 1
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
 
 
 def facto_rec(n: int) -> int:
-    pass
+    if n == 0:
+        return 1
+    return n * facto_rec(n - 1)
 
 
 def fibo_rec(n: int) -> int:
-    pass
+    if n < 2:
+        return n
+    return fibo_rec(n - 1) + fibo_rec(n - 2)
 
 
 def fibo_ite(n: int) -> int:
-    pass
+    if n < 2:
+        return n
+    a, b = 0, 1
+    for _ in range(n - 1):
+        a, b = b, a + b
+    return b
 
 
 def golden_phi(n: int) -> int:
-    pass
-
+    if n == 0:
+        return 1
+    return fibo_ite(n + 1) / fibo_ite(n)
 
 def sqrt5(n: int) -> int:
-    pass
+    phi = golden_phi(n + 1)
+    return 2 * phi - 1
 
 
 def pow(a: float, n: int) -> float:
-    pass
+    return a**n
